0226298c4646beb8b871f86b3c0739325ae51bf5
Administrator <350896367@qq.com>
2020-11-18 15:34:14 +0800

    手机企业微信 1.搜索聊天群，发消息 2.外出自动打卡 3.通讯录添加联系人 4.通讯录删除联系人

--- test_appium_workweixin/test_sendmessage.py
+++ test_appium_workweixin/test_sendmessage.py
@@ -3,10 +3,15 @@
 # @time  :2020/11/16:10:12
 # @Author:啊哩哩
 # @File  :test_sendmessage.py
+from time import sleep
+
+import pytest
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+
+from test_appium_workweixin.conftest import contact
 
 
 class TestWorkWeixin:
@@ -32,26 +37,26 @@ class TestWorkWeixin:
         # 微信已登录，已经在微信认证，企业微信在后台运行中
         # 执行下面这句后，弹出微信登录页面
         # MobileBy.XPATH这种写法是为了封装方便
-        # self.driver.find_element(MobileBy.XPATH, "//*[@text='微信登录']").click()
-        # self.driver.implicitly_wait(10)
+        # self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'微信登录')]").click()
+        # self.driver.implicitly_wait(5)
         # # 用uiautomator定位到“进入企业 ”后边居然有空格，怪不得执行到这里老提示no Element
-        # self.driver.find_element(MobileBy.XPATH, "//*[@text='进入企业 ']").click()
+        # self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'进入企业')]").click()
         # self.driver.implicitly_wait(10)
 
 
     def teardown(self):
-        pass
-        # self.driver.quit()
+        self.driver.quit()
 
     def test_sendmessage(self):
         """
         搜索聊天群，在群里发消息
         :return:
         """
-        # 为了不使用uiautomator方式，只能先查duofu，然后再点duofu，进入聊天页面
-        # 为什么不用uiautomator方式，想少封装一个方法
+        # 先确定聊天群是否存在：查duofu群，然后再点duofu，进入聊天页面
         # 进入搜索框，搜索duofu，进入duofu的聊天窗口
         # 老师说com.tencent.wework:id/ 可以去掉
+        # com.tencent.wework:id/guu这样的id命名，没有具体含义，是动态id
+        # 不要用动态id定位，换手机，id会变，代码得重写
         self.driver.find_element(MobileBy.ID, "guu").click()
         searchname = "duofu"
         self.driver.find_element(MobileBy.XPATH, "//*[@text='搜索']").send_keys(searchname)
@@ -59,8 +64,6 @@ class TestWorkWeixin:
         locator = (MobileBy.ID, "dh2")
         WebDriverWait(self.driver, 60).until(expected_conditions.element_to_be_clickable(locator))
         self.driver.find_element(*locator).click()
-        # 选中聊天输入框
-        self.driver.find_element(MobileBy.ID, "b0z").click()
         # 输入聊天内容，并发送
         sendmessage = "test003"
         self.driver.find_element(MobileBy.ID, "dx1").send_keys(sendmessage)
@@ -92,6 +95,86 @@ class TestWorkWeixin:
         result = self.driver.find_element(MobileBy.ID, "mn").text
         # 判断打卡结果
         assert result == "外出打卡成功"
+
+    def test_addresslist(self):
+        # 首页进入通讯录页面
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']").click()
+
+    @pytest.mark.parametrize("name, gender, phone", contact)
+    def test_addcontact(self, name, gender, phone):
+        """
+        添加联系人
+        :return:
+        """
+        # 首页进入通讯录页面
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']").click()
+        # 考虑到成员很多的时候，底部导航栏需要滚动到下面才能看到，所以做了个滚动操作
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector().'
+                                 'scrollable(true).instance(0)).'
+                                 'scrollIntoView(new UiSelector().text("添加成员")'
+                                 '.instance(0))').click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='手动输入添加']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'姓名')]/../*[@text='必填']").send_keys(name)
+        self.driver.implicitly_wait(3)
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='男']").click()
+        if gender == "女":
+            self.driver.find_element(MobileBy.XPATH, "//*[@text='女']").click()
+        else:
+            # 显式等待10秒
+            element = WebDriverWait(self.driver, 10).until(lambda x:x.find_element(MobileBy.XPATH, "//*[@text='男']"))
+            element.click()
+        # 手机名称对应输入框的定位
+        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'手机') and @class='android.widget.TextView']/..//*[@class='android.widget.EditText']").send_keys(phone)
+        # 点击保存，保存联系人
+        self.driver.find_element(MobileBy.XPATH, "//*[contains(@text,'保存')]").click()
+        # 获取toast提示并判断
+        mytoast = self.driver.find_element(MobileBy.XPATH, "//*[@class='android.widget.Toast']").text
+        assert mytoast == "添加成功"
+        # 退出添加成员页面,有时候toast提示显示较慢,需要等待几秒
+        self.driver.implicitly_wait(3)
+        self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/gu_").click()
+
+
+
+    def test_delecontact(self):
+        """
+        删除联系人
+        :return:
+        """
+        searchname = "liche"
+        # 首页进入通讯录页面
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']").click()
+        # 查询要删除的联系人
+        self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/guu").click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='搜索']").send_keys(searchname)
+        # 手机企业微信这里查询结果出现 比较慢
+        # 搜索所有姓名=liche
+        locator1 = (MobileBy.XPATH, f"//*[@text='{searchname}']")
+        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_all_elements_located(locator1))
+        elements = self.driver.find_elements(*locator1)
+        if len(elements) < 2:
+            print("要删除的联系人不存在")
+            return
+
+        elements[1].click()
+        # 点击个人信息
+        self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/guk").click()
+        # 没有等待时间,编辑成员按钮加载出来了也不点,不懂为什么
+        locator2 = (MobileBy.XPATH, "//*[@text='编辑成员']")
+        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_all_elements_located(locator2))
+        elements2 = self.driver.find_element(*locator2)
+        elements2.click()
+        # 考虑部分手机需要滚动到下面才能看到，所以做了个滚动操作
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector().'
+                                 'scrollable(true).instance(0)).'
+                                 'scrollIntoView(new UiSelector().text("删除成员")'
+                                 '.instance(0))').click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='确定']").click()
+        deleelement = self.driver.find_elements(MobileBy.XPATH, f"//*[@text='{searchname}']")
+        assert len(deleelement) == len(elements)-1
+
 
 
 
